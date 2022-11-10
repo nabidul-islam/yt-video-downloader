3290ac39fc5e6dc2fdcbe063fa8d487e573acf40
nabidul-islam <115966853+nabidul-islam@users.noreply.github.com>
2022-11-10 18:49:09 +0600

    Add files via upload

---
+++ ytvideodownloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+link = input("Enter your youtude url : ")
+yt = YouTube(link)
+print(yt.title)
+videos = yt.streams.all()
+
+video = list(enumerate(videos))
+
+for i in video:
+    print(i)
+
+print ("enter the desired option to download the format")
+
+dn_option = int(input("enter the option : "))
+
+dn_video = videos[dn_option]
+dn_video.download()
+
+print("Downloaded successfully")
